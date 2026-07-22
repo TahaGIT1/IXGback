eeb8e44ef43e51ef8c0b897f781c96bde473a2f2
TahaGIT1 <tahaa.sajan@gmail.com>
2026-07-23 04:01:35 +0530

    Await confirmation email3

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,35 +1,26 @@
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 export async function sendConfirmationEmail(email, name, run) {
-  const info = await transporter.sendMail({
+  console.log("📧 sendConfirmationEmail() started");
+
+  const mailOptions = {
     from: `"IXG Run Club" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "🎉 Registration Confirmed",
     html: `
       <h2>Hello ${name} 👋</h2>
-
       <p>Your registration has been confirmed.</p>
-
-      <hr>
-
       <p><b>Date:</b> ${run.date}</p>
       <p><b>Time:</b> ${run.time}</p>
       <p><b>Location:</b> ${run.location}</p>
       <p><b>Distance:</b> ${run.distance}</p>
-
-      <hr>
-
-      <h3>See you Sunday! 🏃</h3>
     `,
-  });
+  };
 
-  console.log("Email sent:", info.messageId);
+  console.log("📧 About to call transporter.sendMail()");
+
+  const info = await transporter.sendMail(mailOptions);
+
+  console.log("📧 transporter.sendMail() finished");
+  console.log(info);
+
+  return info;
 }
