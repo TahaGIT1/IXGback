7d4cac9091abf0d2efd7bb2fa725104df2ddaeb6
TahaGIT1 <tahaa.sajan@gmail.com>
2026-07-23 04:05:43 +0530

    Fix sendEmail transporter

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,3 +1,13 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
 export async function sendConfirmationEmail(email, name, run) {
   console.log("📧 sendConfirmationEmail() started");
 
@@ -7,11 +17,15 @@ export async function sendConfirmationEmail(email, name, run) {
     subject: "🎉 Registration Confirmed",
     html: `
       <h2>Hello ${name} 👋</h2>
+
       <p>Your registration has been confirmed.</p>
+
       <p><b>Date:</b> ${run.date}</p>
       <p><b>Time:</b> ${run.time}</p>
       <p><b>Location:</b> ${run.location}</p>
       <p><b>Distance:</b> ${run.distance}</p>
+
+      <h3>See you Sunday! 🏃</h3>
     `,
   };
 
@@ -20,7 +34,7 @@ export async function sendConfirmationEmail(email, name, run) {
   const info = await transporter.sendMail(mailOptions);
 
   console.log("📧 transporter.sendMail() finished");
-  console.log(info);
+  console.log(info.messageId);
 
   return info;
 }
